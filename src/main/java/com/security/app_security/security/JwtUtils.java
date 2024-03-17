7c44dffbd8f463f01822ad5a0b479b818de03e40
Jeremias Adriano <109630558+jeremiasadriano@users.noreply.github.com>
2024-03-17 23:32:48 +0200

    Changing new SecretKeySpecif to Keys.hmac

--- src/main/java/com/security/app_security/security/JwtUtils.java
+++ src/main/java/com/security/app_security/security/JwtUtils.java
@@ -1,24 +1,23 @@
 package com.security.app_security.security;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 
 @Component
 public class JwtUtils {
-    private static final String SECRET_KEY = "minhaSenhaSuperSeguraFilhoEntaoREl4X4RNBRother";
-    private static final String ALGORITHM = "HmacSHA512";
+    private static final String SECRET_KEY = "5d67276d6b236e2fe0e0979ba8c4e20f206b006b9d35ada86a2772451fbc12fd";
     private static final Long EXPIRATION_TIME = 1000000L;
     private final SecretKey key;
 
     public JwtUtils() {
-        byte[] bytes = SECRET_KEY.getBytes(StandardCharsets.UTF_8);
-        this.key = new SecretKeySpec(bytes, ALGORITHM);
+        byte[] bytes = Decoders.BASE64URL.decode(SECRET_KEY);
+        this.key = Keys.hmacShaKeyFor(bytes);
     }
 
     public String generateToken(String username) {
