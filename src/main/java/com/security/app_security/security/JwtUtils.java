aea77054d9c56d3e36e8230f5fd4b99489720a45
Jeremias Adriano <109630558+jeremiasadriano@users.noreply.github.com>
2024-03-18 23:34:28 +0200

    Finally i maneged to understand JWT

--- src/main/java/com/security/app_security/security/JwtUtils.java
+++ src/main/java/com/security/app_security/security/JwtUtils.java
@@ -1,23 +1,25 @@
 package com.security.app_security.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.function.Function;
 
 
 @Component
 public class JwtUtils {
     private static final String SECRET_KEY = "5d67276d6b236e2fe0e0979ba8c4e20f206b006b9d35ada86a2772451fbc12fd";
     private static final Long EXPIRATION_TIME = 1000000L;
-    private final SecretKey key;
 
-    public JwtUtils() {
+    private SecretKey key() {
         byte[] bytes = Decoders.BASE64URL.decode(SECRET_KEY);
-        this.key = Keys.hmacShaKeyFor(bytes);
+        return Keys.hmacShaKeyFor(bytes);
     }
 
     public String generateToken(String username) {
@@ -25,17 +27,31 @@ public class JwtUtils {
                 .subject(username)
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(key)
+                .signWith(key())
                 .compact();
     }
 
-    public String extractUsername(String token) {
+    public Claims extractAllClaims(String token) {
         return Jwts.parser()
-                .verifyWith(key)
+                .verifyWith(key())
                 .build()
                 .parseSignedClaims(token)
-                .getPayload()
-                .getSubject();
+                .getPayload();
     }
 
+    public <T> T extractClaim(String token, Function<Claims, T> converter) {
+        return converter.apply(extractAllClaims(token));
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Boolean isValid(String token, UserDetails details) {
+        return (extractUsername(token).equals(details.getUsername()) && !isExpired(token));
+    }
+
+    private Boolean isExpired(String token) {
+        return extractClaim(token, Claims::getExpiration).before(new Date());
+    }
 }
