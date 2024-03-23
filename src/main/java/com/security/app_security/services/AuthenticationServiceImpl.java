d1a5cf69d33a375f1fac7c1796f24014977ffe49
Jeremias Adriano <109630558+jeremiasadriano@users.noreply.github.com>
2024-03-23 23:28:37 +0200

    Validating the email using regex

--- src/main/java/com/security/app_security/services/AuthenticationServiceImpl.java
+++ src/main/java/com/security/app_security/services/AuthenticationServiceImpl.java
@@ -1,19 +1,22 @@
 package com.security.app_security.services;
 
 import com.security.app_security.models.Person;
-import com.security.app_security.models.enums.Roles;
 import com.security.app_security.models.dto.AuthenticationRequest;
 import com.security.app_security.models.dto.AuthenticationResponse;
 import com.security.app_security.models.dto.PersonRegister;
 import com.security.app_security.models.dto.PersonResponse;
+import com.security.app_security.models.enums.Roles;
 import com.security.app_security.repositories.PersonRepository;
 import com.security.app_security.security.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.InputMismatchException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 @RequiredArgsConstructor
@@ -22,18 +25,22 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
     private final PasswordEncoder passwordEncoder;
+    private final Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+@[a-z]{3,6}[.][a-z]+$");
 
     @Override
     public AuthenticationResponse register(PersonRegister register) {
         if (register == null || register.name().isEmpty()) throw new NullPointerException("Null values not allowed!");
+        Matcher matcher = pattern.matcher(register.email());
+        if (!matcher.find()) throw new InputMismatchException("Email invalid!");
         Person person = new Person(register.name(), register.email(), this.passwordEncoder.encode(register.password()), Roles.USER);
         return new AuthenticationResponse(this.jwtService.generateToken(register.email()), new PersonResponse(this.personRepository.save(person)));
     }
 
     @Override
     public AuthenticationResponse login(AuthenticationRequest request) {
+        Matcher matcher = pattern.matcher(request.email());
+        if (!matcher.find()) throw new InputMismatchException("Email invalid!");
         var auth = this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.email(), request.password()));
-        if (!auth.isAuthenticated()) throw new UsernameNotFoundException("This user isn't ours");
         Person person = this.personRepository.findPersonByEmail(request.email()).orElseThrow(NullPointerException::new);
         return new AuthenticationResponse(this.jwtService.generateToken(request.email()), new PersonResponse(person));
     }
